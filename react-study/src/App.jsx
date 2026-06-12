40e9c43c275198d8ffbf76decbc9614b84ae0f4a
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-06-12 11:03:09 +0900

    리액트 초회 수업내용

---
+++ react-study/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Basic01 from "./components/Basic01";
+import State01 from "./components/State01";
+import Export01 from "./components/Export01";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        // <Basic01 />
+        // <State01 />
+        <Export01 />
+    );
+}
+
+export default App;
